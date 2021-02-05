0507f9768b50e5a67b35d2ef85f30a82e64d7c66
FatihDursunUzer1 <fatihdursunuzer@gmail.com>
2021-02-06 02:36:21 +0300

    Student,Donation ve Institution yapısı oluşturuldu.

--- lib/models/Institution.dart
+++ lib/models/Institution.dart
@@ -4,4 +4,22 @@ class Institution {
   String _numberOfStudentsIntheInstitution;
   String _contact;
   String _adress;
+
+  Institution.fromMap(Map<String, dynamic> parsedMap)
+      : _name = parsedMap["name"],
+        _websiteUrl = parsedMap["websiteurl"],
+        _numberOfStudentsIntheInstitution =
+            parsedMap["numberofstudentsintheinstitution"],
+        _contact = parsedMap["contact"],
+        _adress = parsedMap["adress"];
+
+  Map<String, dynamic> toMap() {
+    return {
+      "name": _name,
+      "websiteurl": _websiteUrl,
+      "numberofstudentsintheinstitution": _numberOfStudentsIntheInstitution,
+      "contact": _contact,
+      "adress": _adress,
+    };
+  }
 }
