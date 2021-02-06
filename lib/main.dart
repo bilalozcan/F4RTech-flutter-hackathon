a9e4cdbe40e4d2920bc8d62d3a1008c87a8e206c
Bilal Özcan <bilalozcan015@gmail.com>
2021-02-06 12:50:31 +0300

    automatic user control added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:education/services/authentication.dart';
+import 'package:education/ui/home_page/home_page.dart';
 import 'package:education/ui/landing_page/landing_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
@@ -10,15 +12,26 @@ void main() async{
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  Authentication _authentication = Authentication();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: LandingPage());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder(
+        future: _authentication.currentUser(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData)
+            return HomePage();
+          else
+            return LandingPage();
+        },
+      ),
+    );
   }
 }
